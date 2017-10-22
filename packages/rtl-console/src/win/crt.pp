c7ed77d9e2b5bcf4152022c5554ea50a6b2068fd
James Richters <james@productionautomation.net>
2017-10-22 13:00:38 -0400

    Fix incorrect type for CtrlKeyState in Function RemapScanCode
    CtrlKeyState is passed from KeyEvent.dwControlKeyState and was originally a Byte, however according to the documenation of dwControlkeyState here:
    https://docs.microsoft.com/en-us/windows/console/key-event-record-str
    
    dwControlKeyState is a word variable. 
    I encountered a range check error when pushing the right arrow key and found that the dwControlKeyState being received was $0120 thus putting it out of range of a byte.
    
    with CtrlKeyState propertly defined as a Word, the function now opertes correctly and does not produce a range check error, and also returns the correct remapping of the key.

--- packages/rtl-console/src/win/crt.pp
+++ packages/rtl-console/src/win/crt.pp
@@ -289,7 +289,7 @@ var
    DoingNumChars: Boolean;
    DoingNumCode: Byte;
 
-Function RemapScanCode (ScanCode: byte; CtrlKeyState: byte; keycode:longint): byte;
+Function RemapScanCode (ScanCode: byte; CtrlKeyState: word; keycode:longint): byte;
   { Several remappings of scancodes are necessary to comply with what
     we get with MSDOS. Special Windows keys, as Alt-Tab, Ctrl-Esc etc.
     are excluded }
